340d06a24075bcf6a3658b74d2dabf4c663f6ca2
André Neves <andreneves3@gmail.com>
2021-04-26 01:15:11 -0300

    feat: open challenge page from quiz card

--- lib/src/presenter/home/components/body/quiz_card/quiz_card_widget.dart
+++ lib/src/presenter/home/components/body/quiz_card/quiz_card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/src/presenter/challenge/challenge_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../core/core.dart';
@@ -12,6 +13,47 @@ class QuizCardWidget extends StatelessWidget {
     required this.quiz,
   }) : super(key: key);
 
+  void _openQuiz(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ChallengePage(quiz: quiz),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final borderRadius = BorderRadius.circular(10);
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(
+          BorderSide(color: AppColors.border),
+        ),
+        borderRadius: borderRadius,
+        // color: AppColors.white,
+      ),
+      child: Material(
+        borderRadius: borderRadius,
+        child: InkWell(
+          borderRadius: borderRadius,
+          onTap: () => _openQuiz(context),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: QuizCardContentWidget(quiz),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizCardContentWidget extends StatelessWidget {
+  const QuizCardContentWidget(this.quiz);
+
+  final Quiz quiz;
+
   String get progressInfo {
     return '${quiz.questionsAnswers.length} de ${quiz.questions.length}';
   }
@@ -22,37 +64,32 @@ class QuizCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        border: Border.fromBorderSide(
-          BorderSide(color: AppColors.border),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 40,
+          width: 40,
+          child: Image.asset(AppImages.getIconAssetUrl(quiz.assetImage)),
         ),
-        borderRadius: BorderRadius.circular(10),
-        color: AppColors.white,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 40,
-            width: 40,
-            child: Image.asset(AppImages.data),
-          ),
-          SizedBox(height: 24),
-          Text(quiz.title, style: AppTextStyles.heading2_15),
-          SizedBox(height: 24),
-          Row(
-            children: [
-              Text(progressInfo, style: AppTextStyles.body11),
-              SizedBox(width: 21),
-              Expanded(
-                child: AppLinearProgressIndicatorWidget(value: progressPercent),
-              ),
-            ],
-          )
-        ],
-      ),
+        SizedBox(height: 24),
+        Text(
+          quiz.title,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: AppTextStyles.heading2_15,
+        ),
+        SizedBox(height: 24),
+        Row(
+          children: [
+            Text(progressInfo, style: AppTextStyles.body11),
+            SizedBox(width: 21),
+            Expanded(
+              child: AppLinearProgressIndicatorWidget(value: progressPercent),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
